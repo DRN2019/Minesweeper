c51f9799da79c77f68e7efe4f26e2616fd8eb34c
DRN2019 <darrenhoho@hotmail.com>
2021-09-17 14:14:54 -0400

    Initial Push using IntelliJ

--- src/Board.java
+++ src/Board.java
@@ -4,9 +4,11 @@ public class Board{
     int mineCt; // Number of mines
     int boardX; // Width of Board
     int boardY; // Height of Board
+    int minesLeft;
 
     boolean[][] board = new boolean[boardX][boardY]; // 2D Boolean Array of which tiles have mines
     boolean[][] visible = new boolean[boardX][boardY]; // 2D Boolean Array of which tiles are uncovered
+    boolean[][] flagged = new boolean[boardX][boardY];
 
     public Board(int mineCt, int boardX, int boardY){
         // Initializing vars
@@ -15,6 +17,7 @@ public class Board{
         this.boardY = boardY;
     }
 
+    // Initializes the mines in the board
     public void initMines(){
         int count = 0;
         while (count < mineCt){
@@ -26,15 +29,68 @@ public class Board{
         }
     }
 
-    public void reveal(int x, int y){
+    // Return function for mines left on the board
+    public int getMinesLeft(){
+        return minesLeft;
+    }
+
+    // Return function for the board
+    public boolean[][] getBoard(){
+        return board;
+    }
+
+    // Return function for visible tiles
+    public boolean[][] getVisible(){
+        return visible;
+    }
+
+    // Return function for flagged tiles
+    public boolean[][] getFlagged(){
+        return flagged;
+    }
+
+    // Function that counts the number of mines surrounding a tile
+    // Returns an int of how many surrounding tiles
+    public int surround(int x, int y){
+        int count = 0;
+        for(int i = -1; i <= 1; i++){
+            for(int j = -1; j <= 1; j++){
+                if(!(x + i < 0 || x + i >= boardX) && !(y + j < 0 || y + j >= boardY)){
+                    if(board[x + i][y + j]){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    // Function that interacts with the user's uncovering of tiles
+    // Returns true if user clicked on a mine; Returns false of user didn't
+    public boolean uncover(int x, int y){
+        // Uncovered mine tile
+        if(!flagged[x][y]){
+            if(board[x][y]){
+                return true;
+            }
+            else{
+                reveal(x,y);
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // Reveals the tiles and also recursively clears blank tiles if surrounding mine count is 0
+    public void reveal(int x, int y) {
         // Reveal Part
         visible[x][y] = true;
 
         // Recursively Clearing Blank Tiles
-        if(surround(x,y) == 0){
-            for(int i = -1; i <= 1; i++){
-                for(int j = -1; j <= 1; j++){
-                    if(!(x + i < 0 || x + i > boardX) && !(y + j < 0 || y + j > boardY)){
+        if (surround(x, y) == 0) {
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    if (!(x + i < 0 || x + i > boardX) && !(y + j < 0 || y + j > boardY)) {
                         reveal(x + i, y + i);
                     }
                 }
@@ -42,21 +98,19 @@ public class Board{
         }
     }
 
-    public int surround(int x, int y){
-        int count = 0;
-        for(int i = -1; i <= 1; i++){
-            for(int j = -1; j <= 1; j++){
-                if(!(x + i < 0 || x + i > boardX) && !(y + j < 0 || y + j > boardY)){
-                    if(board[x + i][y + j]){
-                        count++;
-                    }
-                }
+    // Allows the user to flag or unflag a tile as a mine
+    public void flag(int x, int y){
+        // If already flagged
+        if(minesLeft > 0) {
+            if (flagged[x][y]) {
+                flagged[x][y] = false;
+                minesLeft++;
+            } else {
+                flagged[x][y] = true;
+                minesLeft--;
             }
         }
-
-        return count;
     }
-
 
 
 }
--- src/main.java
+++ src/main.java
@@ -8,6 +8,20 @@ public class main{
 
         while(!gameOver){
 
+
+            gameOver = true;
         }
+
+        /*
+        In the start, the game has tto ask the user which gamemode they want to play on
+        The parameters for each game mode are as follows:
+        Easy: 9x9 10 mines
+        Medium: 16x16 40 mines
+        Advanced: 24x24 99 mines
+
+        On the first move, the user should be guaranteed a safe square. Either initialize the mines AFTER the user clicks or initialize the mines before the user makes their first move and move the mine to a random spot if the user happens to click on a mine
+        If it initializes the mines after the first move, then it might lag for a bit once the user makes the first mobe, especially if the init algoritgm is slow. On the other hand, initializing it before can cause some problems with only clearing 1 tile on the first move.
+
+         */
     }
 }
